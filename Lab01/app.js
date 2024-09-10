53718a28e008dcc4b1ad7c0ce1f37e2bce56e95e
Onyxeye03 <129223839+Onyxeye03@users.noreply.github.com>
2024-09-09 20:48:08 -0400

    a

--- Lab01/app.js
+++ Lab01/app.js
@@ -1,13 +1,15 @@
-function RandArray() {
+function fillArrayWithRandomIntegers() {
     const randomNumbers = [];
 
     for (let i = 0; i < 5; i++) {
-        const randomNumber = Math.floor(Math.random() * 100); // Generates a random integer between 0 and 99
+        const randomNumber = Math.floor(Math.random() * 100);
         randomNumbers.push(randomNumber);
     }
 
     return randomNumbers;
 }
 
-const filledArray = RandArray();
-console.log(filledArray);
+const filledArray = fillArrayWithRandomIntegers();
+
+const outputElement = document.getElementById("output");
+outputElement.innerHTML = filledArray;
